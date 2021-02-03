669c8d08f743a4a58559cb57a3f8e8129fec34e1
Gareth Tribello <gareth.tribello@gmail.com>
2021-02-03 11:15:33 +0000

    Made answers right

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import numpy as np
 
-numbers = {}
-for i in range(2) : 
-  # The nested loop over j here is run for every i value.  Calculations
-  # are thus done for i=0, j=0, then i=0, j=1, then i=1, j=0 and finally i=1, j=1.
-  for j in range(2) : 
-    numbers.add{i*j}
+def genMultiplications(N) : 
+    numbers = set([1])
+    for i in range(N) : 
+       # The nested loop over j here is run for every i value.  Calculations
+       # are thus done for i=0, j=0, then i=0, j=1, then i=1, j=0 and finally i=1, j=1.
+       for j in range(i+1) :
+          numbers.add(i*j)
+    return numbers
     
-print( numbers )
+for i in range(1,10): print( genMultiplications(i) )
